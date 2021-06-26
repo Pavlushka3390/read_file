2aa3e6100a63e051869bcade5c5ea92039b5d4f3
pavel kuzin <pavlushka3390@mail.ru>
2021-06-26 12:05:34 +0300

    Изменен способ получения словаря, добавлена функция получения списка покупок

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-with open("recept.txt", encoding= "utf-8") as file:
-    lines = file.readlines()
+import os
+
+
+def get_cook_book(file_name):
     cook_book = {}
-    for line in lines:
-        line_lst = line.split(' | ')
-        if len(line_lst) < 3 and not line[:1].isdigit() and len(line) != 1:
-            dish = line[:-1]
+    with open(file_name, encoding='utf-8') as file:
+        cook_list = [line.strip() for line in file]
+        for itm, elm in enumerate(cook_list):
+            if elm.isdigit():
+                cook_book[cook_list[itm - 1]] = []
+                for recept in cook_list[itm + 1:itm + int(elm) + 1]:
+                    ingredient_name = recept.split('|')[0]
+                    quantity = int(recept.split('|')[1])
+                    measure = recept.split('|')[2]
+                    cook_book[cook_list[itm - 1]].append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
+    return cook_book
 
-            cook_book[dish] = []
-        if len(line_lst) == 3:
-            cook_book[dish].append({'ingredient_name': line_lst[0], 'quantity': line_lst[1], 'measure': line_lst[2].replace('\n', '')})
 
-print(cook_book, end = "\n")
+
+
+
+def get_shop_list_by_dishes(dishes, cooking_book, person_count):
+    for key in dishes:
+        if key in cooking_book.keys():
+            print(key)
+            for value in cooking_book[key]:
+                value['quantity'] *= person_count
+                print(value)
+            else:
+                pass
+get_shop_list_by_dishes(['Утка по-пекински', 'Омлет'], get_cook_book('recept.txt'), 5)
